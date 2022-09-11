e35e502e88e37686fb82ddefe050ff215cd3531d
scjtqs <86882590+scjtqs2@users.noreply.github.com>
2022-09-11 23:52:56 +0800

    Update nfs.sh

--- nfs.sh
+++ nfs.sh
@@ -1,4 +1,6 @@
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
 helm install mailu-nfs  -n mailu-mailserver  --create-namespace  nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+    --set image.repository=scjtqs/k8s \
+    --set image.tag=nfs-subdir-external-provisioner-v4.0.2 \
     --set nfs.server=10.0.0.10 \
     --set nfs.path=/data/share/k8s/mailu  --set storageClass.name=mailu-nfs  --set storageClass.provisionerName=mailu-nfs
